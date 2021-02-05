5a82b3664add4199ec395297d6b7e65be97d9afb
niobeus <49533965+niobeus@users.noreply.github.com>
2021-02-05 13:03:27 +0700

    Update exec.py

--- frameworks/SNNClassifier/exec.py
+++ frameworks/SNNClassifier/exec.py
@@ -12,6 +12,7 @@ from frameworks.shared.callee import call_run, result, save_metadata, utils
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
+from sklearn.metrics import roc_auc_score
 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -47,6 +48,7 @@ def layer(input, output):
 class MLP(nn.Module):
   def __init__(self, n_input, n_hidden, n_output):
     super(MLP, self).__init__()
+    self.n_output = n_output
     n_hidden = [n_input] + n_hidden
     layers = [layer(n_hidden[i], n_hidden[i+1]) for i in range(len(n_hidden) - 1)]
     layers.append(nn.Sequential(nn.Linear(n_hidden[-1], n_output)))
@@ -79,22 +81,33 @@ class NNClassifier:
         y_pred = self.MLP.forward(x)
 
         # print(y_pred, y.long())
-        loss = self.loss(y_pred, torch.max(y.long(),1)[0])
+        if self.MLP.n_output == 1:
+          # y = y.unsqueeze(1)
+          loss = self.loss(y_pred, y)
+        else:
+          loss = self.loss(y_pred, torch.max(y.long(), 1)[0])
         
         loss.backward()
         self.optimizer.step()
-      # print('Epoch {}: train loss: {}'.format(i, loss.item()))
-    print(torch.max(y.long(),1)[0], y.long())
+      # print('Epoch {}: train loss: {:.5f}'.format(i, loss.item()))
+      # print(y_pred, torch.max(y,1)[0])
+    # print(torch.max(y.long(),1)[0], y.long())
       
   def predict(self, X):
     self.MLP.eval()
     with torch.no_grad():
-      return torch.argmax(self.MLP.forward(X))
+      if self.MLP.n_output == 1:
+        return torch.round(torch.sigmoid(self.MLP.forward(X)))
+      else:
+        return torch.argmax(self.MLP.forward(X))
     
   def predict_proba(self, X):
     self.MLP.eval()
     with torch.no_grad():
-      return nn.functional.softmax(self.MLP.forward(X))
+      if self.MLP.n_output == 1:
+        return torch.sigmoid(self.MLP.forward(X))
+      else:
+        return nn.functional.softmax(self.MLP.forward(X))
 
   def score(self, X_test, y_test, metric='acc'):
     self.MLP.eval()
@@ -135,17 +148,22 @@ def run(dataset, config):
     (_, y_train_counts) = np.unique(y_train, return_counts=True)
     n_input = len(X_train[0])
     n_hidden = len(X_train[0])
-    n_hidden = [n_input, n_input]
+    n_hidden = [n_input, n_input, n_input // 2]
     n_output = len(y_train_counts)
+
+    if n_output > 2:
+      loss = nn.CrossEntropyLoss()
+    else:
+      loss = nn.BCEWithLogitsLoss()
+      n_output = 1
+    # loss = nn.CrossEntropyLoss()
 
     mlp = MLP(n_input=n_input, 
               n_hidden=n_hidden, 
               n_output=n_output)
 
-    batch_size = 40
-    epochs = 20
-
-    loss = nn.CrossEntropyLoss()
+    batch_size = 10
+    epochs = 30
     
     net = estimator(mlp, 
                   batch_size=batch_size, 
@@ -160,13 +178,39 @@ def run(dataset, config):
 
     with utils.Timer() as predict:
       predictions = []
+      idx = 0
+      count = 0
       for x in X_test:
         x = torch.Tensor(x).to('cuda:0')
-        pred_x = net.predict(x).to('cpu')
-        predictions.append(int(pred_x))
+        pred_x = net.predict(x).to('cpu').tolist()
+        predictions.append(pred_x)
+        
+        # if int(y_test[idx]) == int(pred_x):
+        #   count += 1 
+        # idx += 1
+
     xx_test = torch.Tensor(X_test).to('cuda:0')
     probabilities = net.predict_proba(xx_test).detach().to('cpu') if is_classification else None
     probabilities = probabilities.tolist()
+        
+    # print('TEST ACCURACY', count/len(y_test))
+    if n_output==1:
+      auc = roc_auc_score(y_test, predictions)
+      print('TEST AUC ', auc)
+    else:
+      res = []
+      [res.extend(l) for l in y_test]
+      res = list(map(int, res))
+      # print(res)
+      # print(predictions)
+      auc_ovo = roc_auc_score(res, probabilities, multi_class='ovo')
+      auc_ovr = roc_auc_score(res, probabilities, multi_class='ovr')
+      print('TEST AUC OVO {} OVR {}'.format(auc_ovo, auc_ovr))
+    
+    print('TEST LOSS (CrossEntropy) ', net.score(X_test, y_test))
+    print('TEST ACCURACY ', net.score(X_test, y_test))
+    print('TRAIN AND TEST FINISHED')
+
     return result(output_file=config.output_predictions_file,
                   predictions=predictions,
                   truth=y_test,
